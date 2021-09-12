ef9753e034f11bec3e50be13ad783a9218e98928
sushmamh7 <sushmamh732@gmail.com>
2021-09-12 20:35:16 +0530

    my first commit

--- scripts.js
+++ scripts.js
@@ -1,14 +1,11 @@
+ 
 let keys = document.querySelectorAll('.keys');
 let spaceKey = document.querySelector('.space_key');
 let shift_left = document.querySelector('.shift_left');
 let shift_right = document.querySelector('.shift_right');
 let caps_lock_key = document.querySelector('.caps_lock_key');
-let toggle_circle = document.querySelector('.toggle_circle');
-let night_mode = document.querySelector('.night_mode');
 let body = document.querySelector('body');
 let text_input = document.querySelector('.text');
-let change_color = document.querySelector('.change_light_color');
-let colors_input = document.querySelector('.colors_input');
 let keyboard_lights = document.querySelector('.keyboard_lights');
 let keyboard_wrapp = document.querySelector('.keyboard_wrapp');
 
@@ -59,24 +56,4 @@ window.addEventListener('keyup', function(e) {
             keys[i].classList.remove('remove')
         },200)
     }
-})
-
-
-night_mode.addEventListener('click',function() {
-    toggle_circle.classList.toggle('active');
-    body.classList.toggle('active');
-    night_mode.classList.toggle('active');
-    keyboard_wrapp.classList.toggle('active');
-    text_input.classList.toggle('active');
-    change_color.classList.toggle('active');
-    for(let i = 0; i < keys.length; i++) {
-        keys[i].classList.toggle('keys_night')
-    }
-})
-
-colors_input.addEventListener('input',function() {
-    for(let i = 0; i < keys.length; i++) {
-        keys[i].style.color = colors_input.value
-    }
-    keyboard_lights.style.background = colors_input.value;
 })
